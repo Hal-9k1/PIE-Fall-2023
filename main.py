8161f768b4d464f2c8fc86d8d59bbf4a50c33aca
Hal-9k1 <edimea211@gmail.com>
2023-04-21 17:28:46 -0700

    Quick commit

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def create_chassis():
     global chassis_object
     if not chassis_object:
         chassis_object = chassis.QuadChassis(get_robot(), debug_logger, (0, 0), 0)
-        #chassis_object = chassis.TestChassis(get_robot(), (0, 0), 0)
+        #chassis_object = chassis.TestChassis(get_robot(), (0, 0), 0, "medium")
 def create_input_generator():
     global input_generator
     if not input_generator:
@@ -42,24 +42,24 @@ def create_arm():
 
 def autonomous_setup():
     create_debug_logger()
-    #create_chassis()
-    create_arm()
-    create_hand()
-    #chassis_object.turn(math.radians(45))
-    #chassis_object.move((4, 3), 5)
+    create_chassis()
+    #create_arm()
+    #create_hand()
+    chassis_object.turn(math.radians(45))
+    chassis_object.move((4, 3), 5)
     #chassis_object.peripheral_action(arm, lambda x: x.set_goal_height(0.5, 0.5))
 def autonomous_main():
     debug_logger.tick()
-    #chassis_object.update()
+    chassis_object.update()
 def teleop_setup():
     create_debug_logger()
-    #create_chassis()
-    create_arm()
-    create_hand()
+    create_chassis()
+    #create_arm()
+    #create_hand()
     create_input_generator()
 def teleop_main():
     debug_logger.tick()
     input_object = input_generator.generate_gamepad()
-    #chassis_object.update_input(input_object)
+    chassis_object.update_input(input_object)
     arm.update_input(input_object)
     hand.update_input(input_object)
